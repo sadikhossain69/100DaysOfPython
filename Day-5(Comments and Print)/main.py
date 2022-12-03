3de1fd78f1d8d598d5dab07281ebc183c5cbeb87
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-03 17:10:48 +0600

    Day 5

---
+++ Day-5(Comments and Print)/main.py
@@ -0,0 +1,20 @@
+# Harry Please don't change this line
+print("Hello \n World")
+
+# This live is comment
+print(5+5)
+print("This is print statement") # This is comment
+
+'''
+This is multi line comment
+'''
+
+print("Hey, I'm a \"Good Boy\"")
+
+print("Hey", 6, 7, sep="~", end="`")
+
+# Assigning value to variable.
+first = "Sadik"
+last = "Hossain"
+
+print(first, last)
