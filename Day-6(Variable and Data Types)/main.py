be9d14bf5f5113843e894fa5062644984e8fbfdc
sadikhossain69 <sadikhossain433@gmail.com>
2022-12-04 19:33:13 +0600

    Day 6

---
+++ Day-6(Variable and Data Types)/main.py
@@ -0,0 +1,19 @@
+a = 10
+print(a)
+b = "Sadik"
+c = None
+d = True
+print(b)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
